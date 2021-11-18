d9435950dc79eed199991d1f76044abc3d1e5e98
xrinkun <xrinkun00@gmail.com>
2021-11-18 18:16:43 +0800

    try

--- List/circular_linked_list.c
+++ List/circular_linked_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define LENGTH 20
 #define OK 1
 #define ERROR 0
 
@@ -18,49 +17,56 @@ Status insertDataByIndex(cList list, Data data, int index); //插入数据到链
 int main() {
     cList list = createList();
     insertDataByIndex(list, 1, 0);
-    insertDataByIndex(list, 2, 0);
+    insertDataByIndex(list, 2, 1);
+    insertDataByIndex(list, 3, 2);
     printList(list);
     return 0;
 }
 
 cList createList() {
-    cList list = malloc(sizeof(Node)*LENGTH);
+    cList list = (Node *)malloc(sizeof(Node));
     list->data = -1;
     list->next = list;
     return list;
 }
 
 Status insertDataByIndex(cList list, Data data, int index) {
-    if (index<0 && index>LENGTH-1) {
+    if (index<0) {
         printf("index out of range");
         return ERROR;
     }
-    Node *curNode = list->next;
-    Node *prevNode = list;
-    Node *nextNode = NULL;
+    Node *curNode = list;
+    Node *prevNode = NULL;
+    Node *nextNode = list->next;
     for (int i=1;i<=index;i++) {
         prevNode = curNode;
         curNode = curNode->next;
         nextNode = curNode->next;
     }
-    if (!nextNode) nextNode = list;
     Node *tempNode = (Node *)malloc(sizeof(Node));
     tempNode->data = data;
-    tempNode->next = nextNode;
-    prevNode->next = tempNode;
+    tempNode->next = curNode;
+    Node *tailNode = curNode;
+    while (1) {
+        if (tailNode->next == list) {
+            break;
+        } else {
+            tailNode = tailNode->next;
+        }
+    }
+    tailNode->next = tempNode;
     return OK;
 }
 
 Status printList(cList list) {
     Node *curNode = list->next;
-    if (curNode == list->next) {
+    if (list == list->next) {
         printf("list is empty");
         return OK;
     }
-    while (curNode) {
+    while (curNode != list) {
         printf("data: %d\n", curNode->data);
-        curNode = list->next;
-        if (curNode == list) break;
+        curNode = curNode->next;
     }
     return OK;
 }
